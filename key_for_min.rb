242a7ca7a0a914e05d95719222a85bcaaa07418e
Marques Avery <averymarques4@gmail.com>
2020-08-11 15:37:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 require 'pry'
 def key_for_min_value(name_hash)
   n = ""
+  v = 
   name_hash.each do |key, value|
     binding.pry
     if value <= v
       n = key
+    else nil
     end
 
   end
