dbc13b3ea203696e5c68b6d6ffd06de85ffe3697
Marques Avery <averymarques4@gmail.com>
2020-08-11 15:40:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  
+  if name_hash = {}
+    return nil
   n = ""
   v = 1000
   name_hash.each do |key, value|
